115e2f5b7bb056837103813f2d424c54141bcfe1
shaharofer283 <shaharofer28@gmail.com>
2020-10-05 15:57:28 +0300

    Changed OptimizationService to Service

--- app/src/main/java/com/example/ofekshlin/wifioptimizeitor/OptimizationService.java
+++ app/src/main/java/com/example/ofekshlin/wifioptimizeitor/OptimizationService.java
@@ -1,24 +1,29 @@
 package com.example.ofekshlin.wifioptimizeitor;
 
 import android.app.IntentService;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.os.IBinder;
 import android.os.SystemClock;
+import android.support.annotation.Nullable;
 import android.widget.Toast;
 
 import java.util.List;
 
 
-public class OptimizationService extends IntentService {
+public class OptimizationService extends Service {
 
     private WifiManager wifiController;
     private long timeToWait;
 
-    public OptimizationService(){
-        super("OptimizeService");
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     @Override
@@ -29,9 +34,15 @@ public class OptimizationService extends IntentService {
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
+    public void onStart(Intent intent, int startid) {
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
         moveToBetterWifi();
         SystemClock.sleep(timeToWait);
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
     }
 
     private void moveToBetterWifi(){
